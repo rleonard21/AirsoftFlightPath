49958fbe1a369c2f11a079f32aa038a0456b755e
rleonard21 <bl.cirrus@gmail.com>
2018-02-17 00:08:25 -0500

    Added download button for csv

---
+++ scripts/arrayToCSV.js
@@ -0,0 +1,21 @@
+function arrayToCSV (twoDiArray) {
+    //  Modified from: http://stackoverflow.com/questions/17836273/
+    //  export-javascript-data-to-csv-file-without-server-interaction
+    let csvRows = [];
+    for (let i = 0; i < twoDiArray.length; ++i) {
+        for (let j = 0; j < twoDiArray[i].length; ++j) {
+            twoDiArray[i][j] = '\"' + twoDiArray[i][j] + '\"';  // Handle elements that contain commas
+        }
+        csvRows.push(twoDiArray[i].join(','));
+    }
+
+    let csvString = csvRows.join('\r\n');
+    let a         = document.createElement('a');
+    a.href        = 'data:attachment/csv,' + csvString;
+    a.target      = '_blank';
+    a.download    = 'flightPathData.csv';
+
+    document.body.appendChild(a);
+    a.click();
+    // Optional: Remove <a> from <body> after done
+}
